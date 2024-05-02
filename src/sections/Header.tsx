5a2dbdbee098e276cae6561271dd23fea2f6d259
Mmmmerdddd <87622756+Mmmmerdddd@users.noreply.github.com>
2024-05-02 09:09:13 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -11,7 +11,7 @@ const Bonus = styled.button`
   cursor: pointer;
   color: #003c00;
   border-radius: 10px;
-  background: #03ffa4;
+  background: #ff0000;
   padding: 2px 10px;
   font-size: 12px;
   text-transform: uppercase;
